663ee74763b61a8f1f3334122a4cb0b5d095ad30
Mierenga <mike.swierenga@gmail.com>
2015-04-10 20:42:33 -0400

    create the repository

---
+++ VirtualM.java
@@ -0,0 +1,36 @@
+/* 
+    A virtual memory system with page replacement.
+    Takes as input a 
+*/
+
+import java.util.Scanner;
+import java.nio.File;
+
+public static final int MAX_PHYSICAL_M =        16;
+public static final int LOGICAL_ADDRESS_SPACE = 64;
+public static final int PAGE_SIZE =             1;
+
+class VirtualM {
+    public static void main(String[] args) {
+        
+        File("input.data");
+        
+        Scanner scan = new Scanner();
+        
+        scan.
+        
+    }
+}
+
+class PCB {
+    private int pid;
+    private String name;
+    private PageTable pageTable;
+    
+    
+}
+
+class PageTable {
+    int[] logical = new int[MAX_PHYSICAL_M];
+    int[] physical = new int[MAX_PHYSICAL_M];
+}
